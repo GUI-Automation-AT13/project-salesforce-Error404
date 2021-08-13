08c4f8d7f6dadfc2983a5873f442c78d319172de
JessickaMoya <jessicka.moya@gmail.com>
2021-08-13 11:55:40 -0400

    Fix checkstyle

--- src/main/java/salesforce/ui/PageTransporter.java
+++ src/main/java/salesforce/ui/PageTransporter.java
@@ -10,7 +10,6 @@ package salesforce.ui;
 
 import core.selenium.MyWebDriverManager;
 import salesforce.config.EnvironmentConfig;
-import salesforce.ui.pages.cases.CasesPage;
 import salesforce.ui.urls.SalesforceClassicUrl;
 import salesforce.ui.urls.SalesforceUrl;
 
